9bc46bfe10208f86b60512593a99ce43e6e412d6
SteveScott <stevescott517@gmail.com>
2014-09-29 13:59:41 -0400

    Created javascript menu with rollover

--- SteveSite/js/main.js
+++ SteveSite/js/main.js
@@ -1,1 +1,10 @@
+$(document).ready(function(){
+});
 
+$('.NavOptions').hover(function(){
+	console.log("It's hitting!")
+	$(this).css("background-color", "#E0E0E0");
+	}, function() {
+		$(this).css("background-color", "white");
+	}
+	);
