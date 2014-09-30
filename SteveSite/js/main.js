2128f65bc4b41dd86ddc749404b10b5da5d6f631
SteveScott <stevescott517@gmail.com>
2014-09-30 01:44:30 -0400

    jCarousel implemented
    added jcarousel
    added jcarousel nav buttons
    created a second nav below

--- SteveSite/js/main.js
+++ SteveSite/js/main.js
@@ -1,8 +1,27 @@
 $(document).ready(function(){
+	$('.jcarousel').jcarousel({
+		center: false,
+		animation: 'fast',
+		wrap: 'circular'
+
+	});
+
+$('.jcarousel-prev').click(function() {
+    $('.jcarousel').jcarousel('scroll', '-=1');
+	});
+
+$('.jcarousel-next').click(function() {
+    $('.jcarousel').jcarousel('scroll', '+=1');
+	});
+
+});
+
+
+$(function(){
+	
 });
 
 $('.NavOptions').hover(function(){
-	console.log("It's hitting!")
 	$(this).css("background-color", "#E0E0E0");
 	}, function() {
 		$(this).css("background-color", "white");
